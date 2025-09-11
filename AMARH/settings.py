8382a47b96ec3d3655a2d4aeb941da78579e3be7
IgorForceBR0202 <igor201774@outlook.com>
2025-09-11 13:18:25 -0300

    Ajustes

--- AMARH/settings.py
+++ AMARH/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'AMARH.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],  # <- pasta global de templates
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
